7554483b63b5ccc221a3479b7ba96a2c2b32eefd
Michael Navarro Ariza <rotpar@gmail.com>
2023-04-18 22:39:41 -0500

    Agrega serilog para funcionar con aws

--- src/Services/ServicioUno/ServicioUno.API/Program.cs
+++ src/Services/ServicioUno/ServicioUno.API/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Host.UseSerilog(CreateSerilogLogger(builder.Configuration));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,3 +25,17 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+{
+    return new LoggerConfiguration()
+        .WriteTo.Console().ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
+}
+
+public partial class Program
+{
+
+    public static string Namespace = typeof(Program).Assembly.GetName().Name;
+    public static string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+}
