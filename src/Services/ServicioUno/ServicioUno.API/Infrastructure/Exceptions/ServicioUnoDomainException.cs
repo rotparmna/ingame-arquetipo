8c4653c0e9b73140f7dcceed2f79be51252d7ff0
Michael Navarro Ariza <rotpar@gmail.com>
2023-04-18 22:22:25 -0500

    Se agrega el manejo de excepciones

---
+++ src/Services/ServicioUno/ServicioUno.API/Infrastructure/Exceptions/ServicioUnoDomainException.cs
@@ -0,0 +1,14 @@
+﻿namespace ServicioUno.API.Infrastructure.Exceptions;
+public class ServicioUnoDomainException : Exception
+{
+    public ServicioUnoDomainException()
+    { }
+
+    public ServicioUnoDomainException(string message)
+        : base(message)
+    { }
+
+    public ServicioUnoDomainException(string message, Exception innerException)
+        : base(message, innerException)
+    { }
+}
